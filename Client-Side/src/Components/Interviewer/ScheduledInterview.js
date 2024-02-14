2bb082e90f50eaec4e2503ac0700602e5662534b
W2-80242 <vpyadav7860@gmail.com>
2024-02-14 16:08:37 +0530

    Updated

--- Client-Side/src/Components/Interviewer/ScheduledInterview.js
+++ Client-Side/src/Components/Interviewer/ScheduledInterview.js
@@ -1,82 +1,65 @@
 import Navbar from "./Navbar";
-<<<<<<< HEAD
+import { Link } from "react-router-dom";
 import "../css/Sceduleinterview.css"
 
-function ScheduledInterview() {
 
-
-
-
-  
-    return (
-        <>
-        <Navbar/>
-        <div className="card">
-              <div style={{backgroundColor:"whitesmoke"}}>
-                <div class="icard">
-                  <p> Title:-<strong>My Title</strong></p>
-                  <p>Date:-My Date</p>
-                  <p >Status</p>
-                </div> 
-                <div class="icard">
-                  <p> Title:-<strong>My Title</strong></p>
-                  <p>Date:-My Date</p>
-                  <p >Status</p>
-                </div> 
-                <div class="icard">
-                  <p> Title:-<strong>My Title</strong></p>
-                  <p>Date:-My Date</p>
-                  <p >Status</p>
-                </div> 
-              </div>
-          </div>
-=======
 import React,{useState,useEffect} from "react";
 import axios from "axios";
+import { myheaders,getCurrentUserid } from "../routes/auth";
 
 function ScheduledInterview() {
   const [interviewers, setInterviewers] = useState([]);
+  const [scheduled,setScheduled] = useState([])
 
-  useEffect(() => {
-      const headers = myheaders();
+    useEffect(() => {
+         const token = myheaders(); 
+         const id = getCurrentUserid();
+         const apiurl = `http://127.0.0.1:9997/interviewer/InterviewSceduled/${id}`
+          //console.log(apiurl)
+        //console.log(headers)
+        axios.get(apiurl, {headers: token})
+         .then((result)=>{
+         // console.log(result.data);
+          //console.log(result.data.result);
+          if(result.data.status=="success")
+          {
+              const arr =result.data.result
+              //console.log(arr)
+              setScheduled(arr)
+              //console.log(arr)
+          }
+          else if (result.data.status=="error")
+          {
+            alert("Some Problem Occured")
+          }
+          else
+          {
+            alert("Something went wrong")
+          }
+    })
+          .catch(err => console.error(err));
+          
+  }, []);  
 
-      axios.get("http://127.0.0.1:9997/interviewer/InterviewSceduled/:{id}", { headers })
-          .then(res => setInterviewers(res.data.result))
-          .catch(err => console.log(err));
-  }, []);
-  const renderInterviewers = () => {
-    return interviewers.map((interviewer) => (
-        <div className="card" style={{ "width": "18rem", "margin": "10px" }} key={interviewer.Interviewerid}>
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">{interviewer.FirstName} {interviewer.LastName}</h5>
-            </div>
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item">{interviewer.Email}</li>
-                <li className="list-group-item">{interviewer.Address}</li>
-                <li className="list-group-item">{interviewer.CompanyPosition}</li>
-                <li className="list-group-item">{interviewer.QualifiedDegree}</li>
-            </ul>
-            <div className="card-body">
-               <Link to="/login/user/scheduleInterview">Schedule Interview</Link> <br/><br/>
-                <a href="#" className="card-link">Check Feedbacks</a>
-            </div>
-        </div>
+  const renderSceduled = () => {
+    return scheduled.map(item => (
+        <div class="icard">
+          <p> Title:-<strong>{item.Title}</strong></p>
+          <p>Date:-{item.Date}</p>
+      
+        </div>       
     ));
-};
+  };
 
 
 
     return (
         <>
         <Navbar/>
-        
-        <h3>GetInterviewers</h3>
-                <button onClick={() => console.log(interviewers)}>My interviewers button</button>
-                <div className="container">
-                    {renderInterviewers()}
-                </div>
->>>>>>> 8273192e (Updated)
+        <div className="card-container">
+            {scheduled.length>0&& renderSceduled()}
+        </div>
+   
         </>
       );
     }   
